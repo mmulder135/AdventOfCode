b2a7cef4a7266e1373e84e2073418f4b3b72c680
Michael Mulder <mmulder135@gmail.com>
2023-12-09 17:45:54 +0100

    Improve Y2023 d09

--- Year2023/Solutions/Day09.hs
+++ Year2023/Solutions/Day09.hs
@@ -1,5 +1,4 @@
 module Year2023.Solutions.Day09 where
-import Data.Binary.Get (Decoder(Fail))
 
 example :: String
 example = "0 3 6 9 12 15\n1 3 6 10 15 21\n10 13 16 21 30 45"
@@ -13,19 +12,22 @@ sol2 :: String -> Int
 sol2 = sum . map (run . reverse) . parse
 
 run :: [Int] -> Int
-run is  | allZeroes is  = 0
+run is  | all (==0) is  = 0
         | otherwise     = last is + run (getDiffs is)
-    where
-        allZeroes :: [Int] -> Bool
-        allZeroes [] = True
-        allZeroes (0:xs) = allZeroes xs
-        allZeroes _ = False
+
+---------- Alternative using iterate and takeWhile and avoiding reverse
+sol1' :: String -> Int
+sol1' = sum . map (foldr ((+) . last) 0 . getSeq) . parse
+
+sol2' :: String -> Int
+sol2' = sum . map (foldr ((-) . head) 0 . getSeq) . parse
+
+getSeq :: [Int] -> [[Int]]
+getSeq = takeWhile (not . all (==0)) . iterate getDiffs
+----------------------------------
 
 getDiffs :: [Int] -> [Int]
-getDiffs = mapAdjacent (flip (-))
-
-mapAdjacent :: (a -> a -> a) -> [a] -> [a]
-mapAdjacent f xs = zipWith f xs (tail xs)
+getDiffs xs = zipWith (-) (tail xs) xs
 
 parse:: String -> [[Int]]
 parse = map (map read . words) . lines
